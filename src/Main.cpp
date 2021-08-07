4070abefb05b2bbbdd2137253f95a046a17492de
유종원 <selflearncoding@naver.com>
2021-08-07 13:39:30 +0900

    Add files via upload

---
+++ src/Main.cpp
@@ -0,0 +1,25 @@
+﻿#include "Main.h"
+
+auto main(int argc, char** argv)->int {
+  string filePath = "main.yu";
+  if (argc > 1) filePath = argv[1];
+  string sourceCode = readSourceCodeFile(filePath);
+  auto tokenList  = scan(sourceCode);
+  auto syntaxTree = parse(tokenList);
+  auto objectCode = generate(syntaxTree);
+  execute(objectCode);
+  system("pause");
+  return 0;
+}
+
+auto readSourceCodeFile(string filePath)->string {
+  ifstream file(filePath);
+  if (file.fail() || file.is_open() == false) {
+    cout << "Failed to open file: " << filePath << endl;
+    system("pause");
+    exit(1);
+  }
+  string sourceCode;
+  getline(file, sourceCode, '\0');
+  return sourceCode;
+}
---
+++ src/Main.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <tuple>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include "Token.h"
+#include "Node.h"
+#include "Code.h"
+
+using std::get;
+using std::cout;
+using std::endl;
+using std::setw;
+using std::left;
+using std::right;
+using std::tuple;
+using std::string;
+using std::ifstream;
+
+auto scan(string)->vector<Token>;
+auto parse(vector<Token>)->Program*;
+auto generate(Program*)->tuple<vector<Code>, map<string, size_t>>;
+auto execute(tuple<vector<Code>, map<string, size_t>>)->void;
+auto readSourceCodeFile(string filePath)->string;
